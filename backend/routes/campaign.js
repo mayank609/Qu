87f78be4978448c07a56affad1749225d6983107
mayank609 <mayankbansal0915@gmail.com>
2026-03-27 16:29:08 +0530

    fix 400

--- backend/routes/campaign.js
+++ backend/routes/campaign.js
@@ -4,13 +4,14 @@ const { roleGuard } = require('../middleware/roleGuard');
 const ctrl = require('../controllers/campaignController');
 
 // Public
-router.get('/', ctrl.getCampaigns);
-router.get('/:id', ctrl.getCampaign);
-
 // Protected - brand only
+router.get('/brand/my', protect, roleGuard('brand'), ctrl.getMyCampaigns);
 router.post('/', protect, roleGuard('brand'), ctrl.createCampaign);
 router.put('/:id', protect, roleGuard('brand'), ctrl.updateCampaign);
 router.delete('/:id', protect, roleGuard('brand'), ctrl.deleteCampaign);
-router.get('/brand/my', protect, roleGuard('brand'), ctrl.getMyCampaigns);
+
+// Public
+router.get('/', ctrl.getCampaigns);
+router.get('/:id', ctrl.getCampaign);
 
 module.exports = router;
